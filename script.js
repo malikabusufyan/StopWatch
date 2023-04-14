db7c19e83737b234202bb7db57519cbfd42f4f54
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-04-14 18:25:08 +0530

    Final Submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Taking the ID's from the HTML
 let countId = document.getElementById("count");
 let secId = document.getElementById("sec");
 let minId = document.getElementById("min");
 let hrId = document.getElementById("hr");
 
+// Initialising the variables which we have to use in our function to compute time
 var hr = 0;
 var min = 0;
 var sec = 0;
@@ -10,15 +12,18 @@ var count = 0;
 
 var timer = false;
 
+// To start the stopwatch
 function start(){
     timer = true;
     stopwatch();
 }
 
+// To stop the stopwatch
 function stop(){
     timer=false;    
 }
 
+// To reset the stopwatch
 function reset(){
     timer=false;
 
@@ -27,6 +32,7 @@ function reset(){
     sec=0;
     count=0;
 
+    // While reset the value we see should be 00 not 0 so we use string and add this using innerhtml
     hrId.innerHTML="00";
     minId.innerHTML="00";
     secId.innerHTML="00";
@@ -34,27 +40,28 @@ function reset(){
 }
 
 
+// This is the main stopwatch Function Here we call this function recursilvey at every 10millisecond
 function stopwatch(){
 
     if (timer==true){
         count=count+1;
-
+        // For count
         if (count==100){
             sec= sec+1;
             count=0;
         }
-
+        // For sec
         if (sec==60){
             min=min+1;
             sec=0;
         }
-
+        // For min
         if (min==60){
             hr=hr+1;
             min=0;
             sec=0;
         }
-
+        // Converting them into string
         var hrString = hr;
         var minString = min;
         var secString = sec;
@@ -72,7 +79,7 @@ function stopwatch(){
         if (count<10){
             countString="0"+count;
         }
-
+        // To show the values in the real time on screen
         hrId.innerHTML=hrString;
         minId.innerHTML=minString;
         secId.innerHTML=secString;
